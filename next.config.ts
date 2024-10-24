a3a05656736d923980327801e6f5f68016fb948e
ishaan <ishaan.mohan.sinha@gmail.com>
2024-10-23 22:36:27 -0400

    updated package

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactStrictMode: true,
+  assetPrefix: "/chess",
+  basePath: "/chess",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
